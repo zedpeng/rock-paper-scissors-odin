b047877f904a5777597ec63fac8d28019d3e34e2
zedpeng <zach.parent@dal.ca>
2021-05-06 18:25:00 -0300

    Write playRound method to play a single round

--- rps.js
+++ rps.js
@@ -5,9 +5,12 @@ let computerSelection;
 //Main
 playerSelection = window.prompt('Enter \'rock\', \'paper\', or \'scissors\'');
 computerSelection = computerPlay();
+playerSelection = playerSelection.toLowerCase(playerSelection);
 
 console.log(playerSelection);
 console.log(computerSelection);
+
+playRound(playerSelection, computerSelection);
 
 //Functions
 function getRandomIntInclusive(min, max) {
@@ -27,8 +30,29 @@ function computerPlay() {
   }  
 }
 
-/*function playRound (playerSelection, computerSelection) {
-
-}*/
+function playRound (playerSelection, computerSelection) {
+  if(playerSelection === computerSelection){
+    console.log('Tie game!');
+    //break;
+  } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+    console.log('Rock beats Scissors! You win!');
+    //break;
+  } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+    console.log('Paper beats Rock! Computer wins!');
+    //break;
+  } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+    console.log('Paper beats Rock! You win!');
+    //break;
+  } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+    console.log('Scissors beat Paper! Computer wins!');
+    //break;
+  } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+    console.log('Scissors beat Paper! You win!');
+    //break;
+  } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+    console.log('Rock beats Scissors! Computer wins!');
+    //break;
+  }
+}
 
 
